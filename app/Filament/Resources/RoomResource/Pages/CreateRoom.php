fd49828d5e8a4bdb43e8e237be0e349762f3b0d1
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-12 17:46:30 -0600

    Creando el recurso para el CRUD de las salas

---
+++ app/Filament/Resources/RoomResource/Pages/CreateRoom.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\RoomResource\Pages;
+
+use App\Filament\Resources\RoomResource;
+use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRoom extends CreateRecord
+{
+    protected static string $resource = RoomResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('¡Sala Creada!')
+            ->body('La sala ha sido creada exitosamente.');
+    }
+}
---
+++ app/Filament/Resources/RoomResource/Pages/EditRoom.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\RoomResource\Pages;
+
+use Filament\Actions;
+use App\Filament\Resources\RoomResource;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRoom extends EditRecord
+{
+    protected static string $resource = RoomResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('¡Sala Actualizada!')
+            ->body('La sala ha sido actualizada exitosamente.');
+    }
+}
