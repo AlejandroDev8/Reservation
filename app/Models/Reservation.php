75b3e8306b448e966f3d94cbef84c7dbb92c6e54
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-12 18:38:36 -0600

    Agregando relaciones de usuario, sala y distribución al modelo de reservaciones

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -16,4 +16,19 @@ class Reservation extends Model
         'notes',
         'answer',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function distribution()
+    {
+        return $this->belongsTo(Distribution::class);
+    }
 }
