1e7d1b2a14b6c4fea780bea8e0267e00070f919d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-18 17:02:48 -0600

    Creando el recurso y las páginas para el CRUD de reservaciones en el panel de dashboar (usuarios)

---
+++ app/Filament/Dashboard/Resources/ReservationResource/Pages/CreateReservation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\ReservationResource\Pages;
+
+use App\Filament\Dashboard\Resources\ReservationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReservation extends CreateRecord
+{
+    protected static string $resource = ReservationResource::class;
+}
