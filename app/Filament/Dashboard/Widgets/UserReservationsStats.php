1ae60d18353a1b91541ef36a43993fe57049c4f3
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-19 02:55:23 -0600

    Refactorizando el widget de UserReservationsStats

--- app/Filament/Dashboard/Widgets/UserReservationsStats.php
+++ app/Filament/Dashboard/Widgets/UserReservationsStats.php
@@ -2,39 +2,50 @@
 
 namespace App\Filament\Dashboard\Widgets;
 
+use App\Models\User;
 use App\Models\Reservation;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Illuminate\Support\Facades\Auth;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class UserReservationsStats extends BaseWidget
 {
-    protected function getApprovedReservations(Reservation $reservation)
+    protected function getApprovedReservations(User $user)
     {
-        return $reservation->where('status', 'aprobada')->count();
+        $totalApprovedReservations = Reservation::where('user_id', $user->id)
+            ->where('status', 'aprobada')->get()->count();
+
+        return $totalApprovedReservations;
     }
 
-    protected function getPendingReservations(Reservation $reservation)
+    protected function getPendingReservations(User $user)
     {
-        return $reservation->where('status', 'pendiente')->count();
+        $totalPendingReservations = Reservation::where('user_id', $user->id)
+            ->where('status', 'pendiente')->get()->count();
+
+        return $totalPendingReservations;
     }
 
-    protected function getRejectedReservations(Reservation $reservation)
+    protected function getRejectedReservations(User $user)
     {
-        return $reservation->where('status', 'rechazada')->count();
+        $totalRejectedReservations = Reservation::where('user_id', $user->id)
+            ->where('status', 'rechazada')->get()->count();
+
+        return $totalRejectedReservations;
     }
 
     protected function getStats(): array
     {
         return [
-            Stat::make('Reservaciones Aprobadas', $this->getApprovedReservations(new Reservation()))
+            Stat::make('Reservaciones Aprobadas', $this->getApprovedReservations(Auth::user()))
                 ->color('success')
                 ->icon('heroicon-s-check-circle')
                 ->description('Reservaciones aprobadas en total'),
-            Stat::make('Reservaciones Pendientes', $this->getPendingReservations(new Reservation()))
+            Stat::make('Reservaciones Pendientes', $this->getPendingReservations(Auth::user()))
                 ->color('warning')
                 ->icon('heroicon-s-clock')
                 ->description('Reservaciones pendientes en total'),
-            Stat::make('Reservaciones Rechazadas', $this->getRejectedReservations(new Reservation()))
+            Stat::make('Reservaciones Rechazadas', $this->getRejectedReservations(Auth::user()))
                 ->color('danger')
                 ->icon('heroicon-s-x-circle')
                 ->description('Reservaciones rechazadas en total'),
