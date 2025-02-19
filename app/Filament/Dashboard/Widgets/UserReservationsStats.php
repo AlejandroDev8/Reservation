e61ff8ac238154fb14976066f6f4f6e233fbcdfc
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-18 20:14:44 -0600

    Agregando widget de estadísticas de reservaciones del usuario con conteo de estados y mejorando íconos en el widget de estadísticas existente.

--- app/Filament/Dashboard/Widgets/UserReservationsStats.php
+++ app/Filament/Dashboard/Widgets/UserReservationsStats.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace App\Filament\Widgets;
+namespace App\Filament\Dashboard\Widgets;
 
 use App\Models\Reservation;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class ReservationsStats extends BaseWidget
+class UserReservationsStats extends BaseWidget
 {
-
     protected function getApprovedReservations(Reservation $reservation)
     {
         return $reservation->where('status', 'aprobada')->count();
@@ -24,25 +23,21 @@ class ReservationsStats extends BaseWidget
         return $reservation->where('status', 'rechazada')->count();
     }
 
-
     protected function getStats(): array
     {
         return [
             Stat::make('Reservaciones Aprobadas', $this->getApprovedReservations(new Reservation()))
-                ->icon('heroicon-s-check-circle')
-                ->description('Reservaciones aprobadas en total')
                 ->color('success')
-                ->extraAttributes([
-                    'class' => 'cursor-pointer',
-                ]),
+                ->icon('heroicon-s-check-circle')
+                ->description('Reservaciones aprobadas en total'),
             Stat::make('Reservaciones Pendientes', $this->getPendingReservations(new Reservation()))
+                ->color('warning')
                 ->icon('heroicon-s-clock')
-                ->description('Reservaciones pendientes en total')
-                ->color('warning'),
+                ->description('Reservaciones pendientes en total'),
             Stat::make('Reservaciones Rechazadas', $this->getRejectedReservations(new Reservation()))
+                ->color('danger')
                 ->icon('heroicon-s-x-circle')
-                ->description('Reservaciones rechazadas en total')
-                ->color('danger'),
+                ->description('Reservaciones rechazadas en total'),
         ];
     }
 }
